a4f588ad220850e1016c91d00600067ef6e136e9
Neel Shah <neelasheshbhai.shah@sjsu.edu>
2024-12-10 16:06:53 -0800

    UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import LandingPage from './components/LandingPage';
+// import AuthPage from './components/AuthPage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <NavBar />
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
+          {/* <Route exact path="/login" element={<AuthPage />} /> */}
         </Routes>
       </div>
     </Router>
